4c0ace902776fb37223d44d096bf6e39cd4fb2dc
Gabriel Vejar <g.vejarperez@gmail.com>
2022-04-03 22:26:37 -0400

    Cambio vista ruta raiz

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::get('/contacto/{nombre?}/{edad?}', function($nombre= 'oscar', $edad='15'){
