f0ad271e3edf83ca7c071d9bee484db56aa5f81b
Gabriel Vejar <g.vejarperez@gmail.com>
2022-04-11 10:29:44 -0400

    Modificación de vistas al nuevo layout

--- resources/views/eliminar.blade.php
+++ resources/views/eliminar.blade.php
@@ -1,36 +1,38 @@
-@extends('layouts.master')
+@extends('layouts.app')
+
 @section('header')
-<h1>Ingrese los Datos para Eliminar</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>Ingrese los Datos para Eliminar</h1>
+        </div>
+    </div>
+</div>
 @stop
 
 @section('content')
 <div class="container">
-        <div class="row">
-            <div class="col col-lg-5">
-                <form action="{{url('formularioEliminar')}}" method="post">
-                    <div class="mb-3">
-                        <label for="">Eliminar</label>
-                        <input type="text" name="eliminarID" class="form-control" placeholder="ingrese ID a Eliminar">
-                    </div>
-                    <button type="submit" class="btn btn-danger">Eliminar</button>
-                    @if($errors->any())
-                    <p>Ingrese el ID</p>
-                    <hr>
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li>{{$error}}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    @endif
-                </form>
-            </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <form action="{{url('formularioEliminar')}}" method="post">
+                <div class="mb-3">
+                    <label for="">Eliminar</label>
+                    <input type="text" name="eliminarID" class="form-control" placeholder="ingrese ID a Eliminar">
+                </div>
+                <button type="submit" class="btn btn-danger">Eliminar</button>
+                @if($errors->any())
+                <p>Ingrese el ID</p>
+                <hr>
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+            </form>
         </div>
     </div>
-@stop
-
-@section('footer')
-<hr>
-<a href="http://127.0.0.1:8000/actividades">Regresar</a>
+</div>
 @stop
